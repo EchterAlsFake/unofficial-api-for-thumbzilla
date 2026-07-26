e27e1ccd50275f100505a65ea971f4e397320ad9
Johannes Habel <EchterAlsFake@proton.me>
2026-07-26 21:33:05 +0200

    - switched to AGPL - release

--- thumbzilla_api/__init__.py
+++ thumbzilla_api/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ["Client", "Video", "Channel", "Playlist", "Pornstar"]
+__all__ = ["Client", "Video", "Channel", "Playlist", "Pornstar", "DownloadConfigHLS"]
 
-from thumbzilla_api.api import Video, Client, Channel, Playlist, Pornstar
+from thumbzilla_api.api import Video, Client, Channel, Playlist, Pornstar, DownloadConfigHLS
